073cb57f34126f3cafd68afa60316bbfce7348e6
JeremJR <jeremy.jacquier-roux@bonitasoft.org>
2015-04-09 09:35:56 +0000

    add support of armv7l architecture for tests

--- manifests/install_app_server.pp
+++ manifests/install_app_server.pp
@@ -13,7 +13,33 @@
 # [Remember: No empty lines between comments and class definition]
 class bonita_bpm::install_app_server {
 
-  package { $bonita_bpm::params::pkg_list: ensure => installed }
+  case $::architecture {
+    'armv7l': {
+      package{'software-properties-common': ensure => installed}
+      exec { 'install_webupd8team_ppa':
+        command => 'add-apt-repository -y ppa:webupd8team/java && apt-get update',
+        creates => '/etc/apt/sources.list.d/webupd8team-java-trusty.list',
+        require => Package['software-properties-common']
+      }
+      exec { 'install_oracle_java8':
+        command => 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && apt-get install -y oracle-java8-installer',
+        creates => '/usr/lib/jvm/java-8-oracle/jre/bin/java',
+        require => Package['software-properties-common']
+      }
+      package { $bonita_bpm::params::pkg_list:
+        ensure  => installed,
+        require => Exec['install_oracle_java8']
+      }
+      exec { 'fix_java_home':
+        command => "echo JAVA_HOME=/usr/lib/jvm/java-8-oracle > /etc/default/${bonita_bpm::params::pkg_app_srv}",
+        unless  => "grep '^JAVA_HOME=/usr/lib/jvm/java-8-oracle' /etc/default/${bonita_bpm::params::pkg_app_srv}",
+        require => Package[$bonita_bpm::params::pkg_app_srv]
+      }
+    }
+    default: {
+      package { $bonita_bpm::params::pkg_list: ensure => installed }
+    }
+  }
 
   service { $bonita_bpm::params::service_name:
     ensure    => running,
